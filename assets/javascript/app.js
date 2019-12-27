2c95fca28c7d35c4bed3da5e63c8e7250db9c9cb
Fung <koufongso@gmail.com>
2019-12-27 14:02:29 -0800

    added auto start next question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,10 @@
 // question dataBase
 // question:[answer,option1,option2...];
 var questionList = {
-    "What is the capital of United States": ["Washington, D.C.", "New York", "Los Angelas", "San Francisco"],
-    "1+2+3+...+98+99+100=": [5050, 4950, 5000, 5100],
-    "Which of the following is <strong>NOT</strong> a programming language": ["Turing", "Basic", "Fortran", "Pascal"],
-    "Who is the first exploer to circumnagivate the world": ["Ferdinand Magellan", "Vasco da Gama", "Christopher Columbus", "Marco Polo"]
+    "What is the capital of United States": ["assets/images/washington.jpg", ["Washington, D.C.", "New York", "Los Angelas", "San Francisco"]],
+    "1+2+3+...+98+99+100=": ["assets/images/math.jpg", [5050, 4950, 5000, 5100]],
+    "Which of the following is <strong>NOT</strong> a programming language": ["assets/images/turing.jpg", ["Turing", "Basic", "Fortran", "Pascal"]],
+    "Who is the first exploer to circumnagivate the world": ["assets/images/Ferdinand_Magellan.jpg", ["Ferdinand Magellan", "Vasco da Gama", "Christopher Columbus", "Marco Polo"]]
 };
 
 var questions = Object.keys(questionList);
@@ -13,6 +13,7 @@ var index = 0;      // index that used to track current question
 const qTime = 30;       // [s] time for each questoin
 var time = qTime;       //[s] current time
 var timerHandle
+var autoNext
 
 const qTotal = 4;       // total num of questions
 var qRemain = qTotal;
@@ -35,15 +36,16 @@ function newGame() {
 /*  start a new quesiton/ start a new timing
 */
 function nextQuestion() {
+    clearTimeout(autoNext);
     if (++index < qTotal) {
         time = qTime;                                                 //reset timer
         // set up question page layout
-        var options = [...questionList[questions[index]]];          // deep  clone the options
+        var options = [...questionList[questions[index]][1]];          // deep  clone the options
         $('.main_panel').empty();
         $('.main_panel').html(`
             <div class="info">
-                <div class="timer"> Time: ${time} s</div>
-                <div class="qCounter">${index+1}/${qTotal}</div>
+                <div class="timer"> Time Remains: ${time} s</div>
+                <div class="qCounter">${index + 1}/${qTotal}</div>
             </div>
             <div class="question_panel">
                 <div class="question"> ${questions[index]}</div>
@@ -91,7 +93,8 @@ function gameOver() {
     $('.main_panel').html(`
         <h1>Your Score</h1>
         <h2>Correct: ${qCorrect}/${qTotal}</h2>
-        <h2>Correct Rate: ${(qCorrect/qTotal)*100} %</h2>
+        <h2>Incorrect: ${qTotal - qCorrect}/${qTotal}</h2>
+        <h2>Correct Rate: ${(qCorrect / qTotal) * 100} %</h2>
         <button id="btn-restart">Try again</button>
     `);
 }
@@ -105,9 +108,9 @@ function check() {
     // check answer
     console.log("check!");
     var result = "Incorrect";
-    var answer = questionList[questions[index]][0];
+    var answer = questionList[questions[index]][1][0];
     for (var i = 0; i < $('input').length; i++) {
-        if ($($('input')[i]).prop("checked") == true && $('#' + $($('input')[i]).attr("value")).text()==answer) {
+        if ($($('input')[i]).prop("checked") == true && $('#' + $($('input')[i]).attr("value")).text() == answer) {
             console.log("correct!")
             result = "Correct";
             qCorrect++;
@@ -115,11 +118,26 @@ function check() {
         }
     }
 
+    var tNext = 10;
     // display checking page
     $('.main_panel').empty();
     $('.main_panel').html(`
         <h1>${result}</h1>
-        <h5>The correct answer is:${answer}</h5>
+        <h2>Answer:${answer}</h2>
+        <img src=${questionList[questions[index]][0]}>
+        <br>
+        <br>
+        <div class="timer-qNext">Next question in ${tNext} s ...</div>
         <button id="btn-next">Next</button>
     `);
+
+    autoNext = setInterval(function () {
+        $('.timer-qNext').text(`Next question in ${--tNext} s ...`);
+        if (tNext == 0) {
+            clearInterval(autoNext);
+            nextQuestion();
+        }
+    }, 1000);
+
+    // autoNext = setTimeout(nextQuestion,10000) // auto go to next question in 10[s]
 }
